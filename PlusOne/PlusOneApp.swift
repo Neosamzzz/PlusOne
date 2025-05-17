2fbc4c2acea081832fc58febd9e4ada542c550b4
hyberiid <qq1069312708@icloud.com>
2025-05-17 23:59:57 +0800

    Initial Commit

---
+++ PlusOne/PlusOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlusOneApp.swift
+//  PlusOne
+//
+//  Created by  Hyberiid on 2025/5/17.
+//
+
+import SwiftUI
+
+@main
+struct PlusOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlusOneTests/PlusOneTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlusOneTests.swift
+//  PlusOneTests
+//
+//  Created by  Hyberiid on 2025/5/17.
+//
+
+import Testing
+@testable import PlusOne
+
+struct PlusOneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
